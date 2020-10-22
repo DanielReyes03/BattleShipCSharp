5b3ad95c20b80332788f81b3d5fd0486571a645b
DanielReyes03 <Daniel_reyes2003@outlook.com>
2020-10-22 14:46:08 -0600

    se lee la matriz

--- Program.cs
+++ Program.cs
@@ -4,9 +4,31 @@ namespace Proyecto_Naval
 {
     class Program
     {
+        private string barco = "barco";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            tabla();
+        }
+
+        public static void tabla(){
+            Program instancia = new Program();
+            string[,] tablero = new string[10,9];
+            //Comienza la construccion de la tabla
+            for (int f = 1; f < tablero.GetLength(0); f++){
+                Console.Write(" | " + f + " | ");
+                for (int c = 1; c < tablero.GetLength(1); c++){
+                    Console.Write(tablero[c,0]);
+                    //Valor de la tabla inicial
+                    tablero[f,c] =  " " + " - " + " ";
+                    //Posicion de barco
+                    tablero[2, 5] = instancia.barco;
+                   // Console.WriteLine("posicion"+"[ "+ f +","+ c +" ]");
+                    //Como se separara los caracteres
+                    Console.Write(" | " +tablero[f,c]+ " | ");
+                }
+                Console.WriteLine();
+            }
+            //Termina construccion de tabla
         }
     }
 }
